080994e275de46d35eef78aecffeed67e619fe08
PietroSciutto <91790426+PietroSciutto@users.noreply.github.com>
2021-12-08 13:29:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-int main(){
-  return 0;
+int main () {
+     int i, N, max;
+
+     do{
+  	   cout<<"Inserisci la dimensione: ";
+  	   cin>>N;
+      }while(N<=0);
+
+      int a[N];
+
+       for (i=0; i<N; i++) {
+  	   cout<<"Inserisci un numero: ";
+	   cin>>a[i];
+       }
+
+	//troviamo il massimo
+
+	max=a[0]; //poniamo il massimo uguale al primo elemento
+
+  	for (i=0; i<N; i++) {
+  		if(a[i]>max){
+  			max=a[i];
+		  }
+
+	}
+	cout<<max<<endl;
+
+	return 0;
 }
