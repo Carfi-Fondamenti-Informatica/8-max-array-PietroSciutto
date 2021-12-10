2f92606df5701cb043aa5761e26efb166483b7ff
PietroSciutto <91790426+PietroSciutto@users.noreply.github.com>
2021-12-10 21:45:19 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,6 @@
 #ifndef ESERCITAZIONE_8_LIB_H
 #define ESERCITAZIONE_8_LIB_H
 
-float numeroMassimo(const float numeri[], int dim);
+float numeroMassimo(const float numeri[], int dimensione);
 
 #endif //ESERCITAZIONE_8_LIB_H
